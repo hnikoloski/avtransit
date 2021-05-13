e3a3c5d2a53078023ddf76c815974332d822b71a
Hristijan <h.nikoloski@outlook.com>
2021-05-13 15:56:09 +0200

    Map Added

---
+++ template-parts/geo-map.php
@@ -0,0 +1,15 @@
+<div id="map">
+
+    <?= do_shortcode('[leaflet-map zoom=6 zoomcontrol scrollwheel lat=43.65 lng=-79.385]
+
+
+
+[leaflet-marker lat=41.881832 lng=-87.623177 svg background="#762222" iconClass="fas fa-circle" color="#fff" font-size="12px"]
+<div class="info-window-content">
+    <h6>Office Title</h6>
+    <p>Address</p>
+</div>
+[/leaflet-marker]
+
+[leaflet-marker lat=43.67 lng=-79.4]'); ?>
+</div>
