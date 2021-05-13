aa5d5e3123daab2ed3e2d8506b0d69a6c5368bb3
Hristijan <h.nikoloski@outlook.com>
2021-05-13 15:56:18 +0200

    .

--- homepage.php
+++ homepage.php
@@ -16,6 +16,7 @@ require('template-parts/main-services.php');
 require('template-parts/why-section.php');
 require('template-parts/cta-section.php');
 require('template-parts/cta-text.php');
+require('template-parts/geo-map.php');
 require('template-parts/newsletter.php');
 ?>
 
---
+++ services-page.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Template Name: Services
+ *
+ * @package AVtransit
+ */
+
+get_header();
+require('template-parts/single-hero.php');
+$contentImg = get_field('content_image_about');
+?>
+<!-- <div class="container-fluid about-us-container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="content">
+                <h3><?= the_field('content_title_about'); ?></h3>
+                <?= the_field('content_about'); ?>
+            </div>
+        </div>
+        <div class="col-lg-6 img-col" style="background:url('<?php echo esc_url($contentImg['url']); ?>'); background-size:cover;
+        background-position:center; background-repeat:no-repeat;">
+            <h4>asdasd</h4>
+        </div>
+    </div>
+</div> -->
+<?php
+get_footer();
